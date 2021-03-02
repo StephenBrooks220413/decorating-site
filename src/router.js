88fdb644182e55e3aafa474ba04db0cbc43c4672
stephen.brooks220413 <stephen.brooks220413@gmail.com>
2021-03-02 15:01:39 -0800

    update

--- src/router.js
+++ src/router.js
@@ -13,7 +13,7 @@ export default new Router({
       component: Home
     },
     {
-      path: '/',
+      path: '/about',
       name: 'about',
       component: About
     }
---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+      <br /><br />
+      <h1>About Our Service</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    .about {
+        margin-top: 130px;
+    }
+</style>
